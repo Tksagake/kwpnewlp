42003da10e57c900450c56c0ed0010813f10f1f5
Sikolia Malcolm <malcolmsikolia@gmail.com>
2025-05-12 11:36:33 +0300

    mvp 1

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: './src',
+  build: {
+    outDir: '../dist',
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  
 });
